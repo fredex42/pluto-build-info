e1a8665ce5d071e62454892dc5f823df83c24a8a
andy_gallagher <andy.gallagher@theguardian.com>
2022-03-14 17:01:21 +0000

    premerge tidies

--- index.js
+++ index.js
@@ -11,25 +11,6 @@ const octokit = new Octokit({
     userAgent: "pluto-build-info v1.0",
 });
 
-
-/*
-#!/bin/bash
-
-## This simple shell script outputs a build-info YAML file that is used by pluto versions manager
-## to understand where this build came from
-echo ci_commit_branch: "${GITHUB_REF_NAME}" >> build-info.yaml
-echo ci_commit_ref_name: "${GITHUB_REF_NAME}" >> build-info.yaml
-echo ci_commit_sha: "${GITHUB_SHA}" >> build-info.yaml
-echo ci_commit_timestamp: "${CI_COMMIT_TIMESTAMP}" >> build-info.yaml
-echo ci_commit_title: "${CI_COMMIT_TITLE}" >> build-info.yaml
-echo ci_job_url: "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" >> build-info.yaml
-echo ci_project_name: "${GITHUB_REPOSITORY}" >> build-info.yaml
-echo ci_merge_request_project_url: "${CI_MERGE_REQUEST_PROJECT_URL}" >> build-info.yaml
-echo ci_merge_request_title: "${CI_MERGE_REQUEST_TITLE}" >> build-info.yaml
-echo ci_pipeline_iid: "${CI_PIPELINE_IID}" >> build-info.yaml
-echo built_image: guardianmultimedia/deliverable-receiver:$CI_PIPELINE_IID >> build-info.yaml
- */
-
 async function main() {
     const prId = github.context.payload.pull_request?.number;
     const owner = github.context.repo.owner;
@@ -39,11 +20,11 @@ async function main() {
     console.log(`owner is ${owner}`);
     console.log(`repo is ${repo}`);
 
+    /* if you need to work out what the available fields are, insert a `console.log` line for these objects then
+    run the action to see what you get
+     */
     const commitInfo = await octokit.git.getCommit({ owner: owner, repo: repo, commit_sha: github.context.sha });
     const prInfo = prId ? await octokit.pulls.get({owner: owner, repo: repo, pull_number: prId }) : undefined;
-
-    console.log("commitInfo: ", commitInfo);
-    console.log("prInfo: ", prInfo);
 
     try {
         const data = {
